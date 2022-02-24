fc371289a16478259e16d13e7ce1ae3316adcc05
Aleksandras Petrovicevas <aleksandras.petrovicevas@nfq.lt>
2022-02-24 15:51:22 +0200

    feat:

---
+++ src/components/Opa2.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Opa2 = () => {
+  return (
+    <View>
+      <Text>Opa2</Text>
+    </View>
+  )
+}
+
+export default Opa2
